8c4ab105bf7ab88ad0d71c72faa8a21b2d053b71
WMAlves <williamma03@gmail.com>
2023-01-21 12:31:23 -0300

    adicionado altarações html

---
+++ client/js/app/controllers/NegociacaoControllers.js
@@ -0,0 +1,12 @@
+//class NegociacaoController{
+//    
+//
+//    adiciona(event){
+//        event.preventDefault();
+//        alert('Chamei ação no controller');
+//    }
+//
+//
+//}
+
+alert('teste de alert javascript')
