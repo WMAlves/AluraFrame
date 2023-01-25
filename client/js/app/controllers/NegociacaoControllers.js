fabcc5e71ae8ac1abc830663a919f46b27847062
WMAlves <williamma03@gmail.com>
2023-01-24 22:33:41 -0300

    Acrescimo do arquivo NegocicaController.js, criado eventos e seu construtor

--- client/js/app/controllers/NegociacaoControllers.js
+++ client/js/app/controllers/NegociacaoControllers.js
@@ -1,18 +1,30 @@
 class NegociacaoController{
 
+    constructor(){
+        
+        let $ = document.querySelector.bind(document);    
+        this._inputData=$('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
 
     adiciona(event){
         event.preventDefault();
-        
-        let $ = document.getElementById.bind(document);
-        let inputData=$('data');
-        let inputQuantidade = $('quantidade');
-        let inputValor = $('valor');
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        let data = new Date(...
+            this._inputData.value
+                .split('-')                
+                .map((item, indice)=> item - indice%2)
+        );
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+            );
+            console.log(negociacao)
+    
     }
     
-    
+
 }
